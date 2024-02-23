8977bd476ba1c66a1cbd856af043c6f714361014
LeaAntony <159743175+LeaAntony@users.noreply.github.com>
2024-02-23 11:50:22 +0700

    Update DashboardYController.php

--- app/Http/Controllers/DashboardYController.php
+++ app/Http/Controllers/DashboardYController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DashboardY extends Controller
+class DashboardYController extends Controller
 {
     function index() {
         return view('dashboardy');
