a4b325c41b6f1a28ca5454213630059728eeb24d
Lea Antony <tonimemangkeren76@gmail.com>
2024-02-23 11:04:49 +0700

    perbaiki

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,6 @@
 <div>
-<<<<<<< HEAD
     <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
     Selamat datang di home {{$nama}} <br>
     Anda adalah seorang {{$pekerjaan}}
 </div>
 <button><a href="contact">Kontak</a></button>
-=======
-    <!-- When there is no desire, all things are at peace. - Laozi -->
-    Selamat Datang di Home {{$nama}}<br>
-    Anda adalah seorang {{$pekerjaan}}
-
-</div>
->>>>>>> ee6d7a546df556303d86e4f1303efeeb84da0eb1
