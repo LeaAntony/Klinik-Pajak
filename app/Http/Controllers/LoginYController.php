e253fe3da4c78d99e721188f00d4bba33e6bbb42
vania <vanivania2005@gmail.com>
2024-02-23 11:56:58 +0700

    Login y

---
+++ app/Http/Controllers/LoginYController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginYController extends Controller
+{
+    public function index()
+    {
+        return view('LoginY');
+    }
+}
