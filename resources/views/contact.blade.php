d2ac062093e1eebaaca6140f91368cbc4f0d1ba5
AuliaSabrina02 <aauliasabrina14@gmail.com>
2024-02-23 11:58:09 +0700

    Login X

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,10 +1,5 @@
 <div>
-<<<<<<< HEAD
     <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
     Selamat datang di home
 </div>
 <button><a href="../">Kembali</a></button>
-=======
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-</div>
->>>>>>> ee6d7a546df556303d86e4f1303efeeb84da0eb1
