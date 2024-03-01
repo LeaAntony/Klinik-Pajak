b529c86ec00b26e0474d7b4d90a79374142710e2
juanngln <juanjonathan05@gmail.com>
2024-03-01 10:00:28 +0700

    perbaiki lopez

--- app/Http/Controllers/ListBarangLopezController.php
+++ app/Http/Controllers/ListBarangLopezController.php
@@ -11,10 +11,10 @@ class ListBarangLopezController extends Controller
         // Logika untuk mendapatkan array data
         $dataBarang = [
             ['id' => 1, 'nama' => 'Beras Harumas 25 Kg', 'harga' => 350000],
-            ['id' => 1, 'nama' => 'Tepung Rosebrand', 'harga' => 10000],
-            ['id' => 1, 'nama' => 'Sofell Anti Nyamuk', 'harga' => 3000],
-            ['id' => 1, 'nama' => 'Masako', 'harga' => 5000],
-            ['id' => 1, 'nama' => 'Minyak Goreng Sunco 1 Liter', 'harga' => 30000],
+            ['id' => 2, 'nama' => 'Tepung Rosebrand', 'harga' => 10000],
+            ['id' => 3, 'nama' => 'Sofell Anti Nyamuk', 'harga' => 3000],
+            ['id' => 4, 'nama' => 'Masako', 'harga' => 5000],
+            ['id' => 5, 'nama' => 'Minyak Goreng Sunco 1 Liter', 'harga' => 30000],
         ];
 
         return $dataBarang;
@@ -22,6 +22,6 @@ class ListBarangLopezController extends Controller
 
     public function tampilkan() {
         $data = $this->getData();
-        return view ('list_barang', compact('data'));
+        return view ('list_barang_lopez', compact('data'));
     }
 }
