a3fbfda455bd144332093126caf5f2403d72ce54
AuliaSabrina02 <aauliasabrina14@gmail.com>
2024-03-08 11:06:43 +0700

    Tailwind_Aulia

--- resources/views/Aulia_View.blade.php
+++ resources/views/Aulia_View.blade.php
@@ -1,22 +1,30 @@
-<html>
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>Harga</th>
-        </tr>
-</thead>
-<tbody>
-    @foreach($data as $dataku)
-        <tr>
-            <td>{{ $dataku['id'] }}</td>
-            <td>{{ $dataku['nama'] }}</td>
-            <td>{{ $dataku['harga'] }}</td>
-        </tr>
-    @endforeach
-</tbody>
-</table>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+<table class="m-10 border-collapse border border-slate-400 bg-red-50">
+        <thead>
+            <tr>
+                <th class="border border-slate-300">ID</th>
+                <th class="border border-slate-300">Nama</th>
+                <th class="border border-slate-300">Harga</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data as $dataku)
+                <tr>
+                    <td class="border border-slate-300" >{{ $dataku['id'] }}</td>
+                    <td class="border border-slate-300" >{{ $dataku['nama'] }}</td>
+                    <td class="border border-slate-300">{{ $dataku['harga'] }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
 </html>
-              
 
